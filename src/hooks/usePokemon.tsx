f28edd74c8d35c1ef6b9cf081080c68ffa0bc425
dloyolar <diego.loyola@continuumhq.com>
2022-08-04 12:48:16 -0400

    feat: add usePokemon hook

---
+++ src/hooks/usePokemon.tsx
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react';
+import {pokemonApi} from '../api/pokemonApi';
+import {PokemonFull} from '../interfaces/pokemonInterfaces';
+
+export const usePokemon = (id: string) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState<PokemonFull>({} as PokemonFull);
+
+  const loadPokemon = async () => {
+    const {data} = await pokemonApi.get<PokemonFull>(
+      `https://pokeapi.co/api/v2/pokemon/${id}`,
+    );
+    setPokemon(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadPokemon();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {pokemon, isLoading};
+};
