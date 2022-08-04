da10052ce370a41d5f66ff81b5d1e906fc11b103
dloyolar <diego.loyola@continuumhq.com>
2022-08-03 22:17:02 -0400

    wip: working in home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,41 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {ActivityIndicator, FlatList, Image} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
+
+import {styles} from '../theme/appTheme';
+import {FadeInImage} from '../components/FadeInImage';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const {simplePokemonList, loadPokemons} = usePokemonPaginated();
+  console.log(simplePokemonList);
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="rocket-outline" size={30} color="#900" />
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolBG}
+      />
+
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <FadeInImage uri={item.picture} style={{width: 100, height: 100}} />
+        )}
+        // Infinite Scroll
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+        }
+      />
+
+      {/* <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text> */}
+    </>
   );
 };
