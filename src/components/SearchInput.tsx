0523c6ccbb9f8ed218b542ecc6c97b2941471e17
dloyolar <diego.loyola@continuumhq.com>
2022-08-05 22:37:10 -0400

    feat: add SearchInput component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, TextInput, StyleProp, ViewStyle} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useDebouncedValue} from '../hooks/useDebouncedValue';
+
+interface Props {
+  style?: StyleProp<ViewStyle>;
+  onDebounce: (value: string) => void;
+}
+
+export const SearchInput = ({style, onDebounce}: Props) => {
+  const [textValue, setTextValue] = useState('');
+  const debouncedValue = useDebouncedValue(textValue);
+  useEffect(() => {
+    onDebounce(debouncedValue);
+  }, [debouncedValue]);
+
+  return (
+    <View style={{...styles.container, ...(style as any)}}>
+      <View style={styles.textBackground}>
+        <TextInput
+          placeholder="Buscar pokemon"
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={textValue}
+          onChangeText={setTextValue}
+        />
+        <Icon name="search-outline" color="grey" size={30} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: 'red',
+  },
+  textBackground: {
+    backgroundColor: '#F3F1F3',
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
